cd31992e03b033b89a13ac55a535db49f0298404
Mnsaaa94 <7mnsaaa94@gmail.com>
2016-05-16 10:02:04 -0400

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -2,10 +2,10 @@ angular.module('todoApp', [])
 .controller('TodoListController', function($scope) {
 	var todoList = this;
 	todoList.todoItems = [
-		{name: 'Go to School'},
-		{name: 'Eat lunch'},
-		{name: 'Do laundry'},
-		{name: 'Buy materials'}
+		{name: 'Go to School', done: false},
+		{name: 'Eat lunch', done: false},
+		{name: 'Do laundry', done: false},
+		{name: 'Buy materials', done: false}
 	]
 
 	todoList.addTodo = function() {
